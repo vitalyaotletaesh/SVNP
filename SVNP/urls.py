6557bb6818cf5f72e3b91f0a3c7dc5706ff30a9b
VitalyaOtletaesh <xxxamyakov@mail.ru>
2022-06-22 13:38:54 +0500

    Added views, templates, fix modeles

--- SVNP/urls.py
+++ SVNP/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.project_list, name='project_list'),
+    path('project/<int:pk>/', views.project_detail, name='project_detail'),
 ]
--- SVNP/views.py
+++ SVNP/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Project
+
 
 def project_list(request):
     projects = Project.objects.order_by('-created_date')
     return render(request, 'SVNP/project_list.html', {'projects': projects})
+
+
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    return render(request, 'SVNP/project_detail.html', {'project': project})
 
