4cf9518c32bab34fcb0e4c4ee8a264801053384f
VitalyaOtletaesh <xxxamyakov@mail.ru>
2022-06-30 18:28:45 +0500

    fix svn script

--- SVNP/svn_commands_script.py
+++ SVNP/svn_commands_script.py
@@ -4,3 +4,7 @@ import os
 def svn_commit():
     os.system('svn add --force C:\\Users\\xxxam\\DjangoProjects\\media\\project_data\\SVNP')
     os.system('svn commit --message "changed files" C:\\Users\\xxxam\\DjangoProjects\\media\\project_data\\SVNP')
+    os.system('svn add --force /home/VitalyaOtletaesh/vitalyaotletaesh.pythonanywhere.com/media''/project_data/SVNP')
+    os.system('svn commit --message "changed files" /home/VitalyaOtletaesh/vitalyaotletaesh.pythonanywhere.com/media'
+              '/project_data/SVNP')
+
